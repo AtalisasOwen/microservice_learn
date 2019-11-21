28ec4d67719036311f9083dcd1c281503a1ed193
10221903 <742137319@qq.com>
2019-11-21 09:52:15 +0800

    finish Recommendation Service fastly !!!

--- microservices/recommendation-service/src/main/java/se/magnus/microservices/core/recommendation/persistence/RecommendationRepository.java
+++ microservices/recommendation-service/src/main/java/se/magnus/microservices/core/recommendation/persistence/RecommendationRepository.java
@@ -1,16 +1,28 @@
 package se.magnus.microservices.core.recommendation.persistence;
 
+import org.apache.ibatis.annotations.*;
+import org.apache.ibatis.type.JdbcType;
+import org.springframework.stereotype.Repository;
 import se.magnus.api.core.recommendation.Recommendation;
 
 import java.util.List;
-
+@Repository
+@Mapper
 public interface RecommendationRepository {
 
+    @Insert("insert into recommendation(author, content, product_id, recomm_id, rate) values (#{author}, #{content}, #{productId}, #{recommendationId}, #{rate})")
+    void save(Recommendation recomm);
 
-    Recommendation save(Recommendation recomm);
+    @Delete("delete from recommendation where product_id = #{productId}")
+    void deleteAll(int productId);
 
-    void deleteAll(List<Recommendation> recommendations);
-
+    @Select("select * from recommendation where product_id = #{productId}")
+    @Results(
+            value = {
+                    @Result(column = "product_id",property = "productId", javaType = int.class, jdbcType = JdbcType.INTEGER),
+                    @Result(column = "recomm_id",property = "recommendationId", javaType = int.class, jdbcType = JdbcType.INTEGER)
+            }
+    )
     List<Recommendation> findByProductId(int productId);
 
 }
--- microservices/recommendation-service/src/main/java/se/magnus/microservices/core/recommendation/services/RecommendationServiceImpl.java
+++ microservices/recommendation-service/src/main/java/se/magnus/microservices/core/recommendation/services/RecommendationServiceImpl.java
@@ -3,9 +3,11 @@ package se.magnus.microservices.core.recommendation.services;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.web.bind.annotation.RestController;
 import se.magnus.api.core.recommendation.Recommendation;
 import se.magnus.api.core.recommendation.RecommendationService;
+import se.magnus.microservices.core.recommendation.persistence.RecommendationRepository;
 import se.magnus.util.exceptions.InvalidInputException;
 import se.magnus.util.http.ServiceUtil;
 
@@ -18,24 +20,42 @@ public class RecommendationServiceImpl implements RecommendationService {
     private static final Logger LOG = LoggerFactory.getLogger(RecommendationServiceImpl.class);
 
     private final ServiceUtil serviceUtil;
+    private final RecommendationRepository repository;
 
     @Autowired
-    public RecommendationServiceImpl(ServiceUtil serviceUtil) {
+    public RecommendationServiceImpl(ServiceUtil serviceUtil, RecommendationRepository repository) {
+        this.repository = repository;
         this.serviceUtil = serviceUtil;
     }
 
     @Override
     public Recommendation createRecommendation(Recommendation body) {
-        return null;
+        try {
+             repository.save(body);
+
+            LOG.debug("createRecommendation: created a recommendation entity: {}/{}", body.getProductId(), body.getRecommendationId());
+            body.setServiceAddress(serviceUtil.getServiceAddress());
+            return body;
+        } catch (DuplicateKeyException dke) {
+            throw new InvalidInputException("Duplicate key, Product Id: " + body.getProductId() + ", Recommendation Id:" + body.getRecommendationId());
+        }
     }
 
     @Override
     public List<Recommendation> getRecommendations(int productId) {
-        return null;
+        if (productId < 1) throw new InvalidInputException("Invalid productId: " + productId);
+
+        List<Recommendation> list = repository.findByProductId(productId);
+        list.forEach(e -> e.setServiceAddress(serviceUtil.getServiceAddress()));
+
+        LOG.debug("getRecommendations: response size: {}", list.size());
+
+        return list;
     }
 
     @Override
     public void deleteRecommendations(int productId) {
-
+        LOG.debug("deleteRecommendations: tries to delete recommendations for the product with productId: {}", productId);
+        repository.deleteAll(productId);
     }
 }
