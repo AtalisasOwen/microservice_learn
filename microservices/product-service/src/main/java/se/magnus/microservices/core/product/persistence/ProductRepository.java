999e9134e76052072e30617fb86f16ceff88fff2
10221903 <742137319@qq.com>
2019-11-21 08:09:15 +0800

    finish Product Service totally

--- microservices/product-service/src/main/java/se/magnus/microservices/core/product/persistence/ProductRepository.java
+++ microservices/product-service/src/main/java/se/magnus/microservices/core/product/persistence/ProductRepository.java
@@ -1,6 +1,7 @@
 package se.magnus.microservices.core.product.persistence;
 
 import org.apache.ibatis.annotations.*;
+import org.apache.ibatis.type.JdbcType;
 import org.springframework.stereotype.Component;
 import se.magnus.api.core.product.Product;
 
@@ -11,6 +12,11 @@ import java.util.Optional;
 public interface ProductRepository {
 
     @Select("select * from product where product_id = #{productId}")
+    @Results(
+            value = {
+                    @Result(column = "product_id", property = "productId", javaType = Integer.class, jdbcType = JdbcType.INTEGER)
+            }
+    )
     ProductEntity findByProductId(int productId);
 
     @Insert("insert into product(product_id, weight, name) values(#{productId}, #{weight}, #{name})")
